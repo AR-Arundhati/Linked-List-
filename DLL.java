1e6b90f6e19190a91ff3e666bf1ec002f0d74777
AR-Arundhati <155011300+AR-Arundhati@users.noreply.github.com>
2025-07-16 12:59:24 +0530

    Add files via upload

---
+++ DLL.java
@@ -0,0 +1,60 @@
+class Node{
+    int data;
+    Node prev, next;
+    
+    
+    // constructor 
+    
+    Node(int data){
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+    
+}
+public class DLL{
+    
+     Node head;
+    
+    // InsertAtBeginning
+    
+    public void InsertAtBeginning(int data){
+         Node newNode = new Node(data);
+        if(head==null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+         head = newNode;
+    }
+   
+    
+    
+   // display Method 
+   
+   
+  public void display(){
+      Node temp = head;
+      System.out.print("null<->");
+      while(temp!=null){
+          System.out.print(temp.data + " <->");
+          temp = temp.next;
+      }
+      System.out.print("null");
+  }
+
+    public static void main(String[] args){
+        
+        DLL list = new DLL();
+        list.InsertAtBeginning(40);
+        list.InsertAtBeginning(50);
+        list.InsertAtBeginning(60);
+        list.InsertAtBeginning(70);
+        
+        list.display();   
+    }
+}
+
+ 
+
---
+++ SLL.java
@@ -0,0 +1,11 @@
+public void insertAtSpecificPosition(int value, int position){
+    if(positipn<1){
+        System.out.print("invslid position");
+        return;
+    }
+    if(position=1){
+        InsertAtBeginning(value);
+        
+    }
+
+})
